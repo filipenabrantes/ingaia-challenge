08e502ed73bdc5b91360b504f7275678fe729660
Filipe Abrantes <filipenabrantes@gmail.com>
2021-03-15 01:09:43 -0300

    adjust swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { cssSwagger } from './css-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,10 +14,18 @@ async function bootstrap() {
     .setTitle('API 1 - inGaia Challenge')
     .setDescription('Return the square meter value from fake database')
     .setVersion('1.0')
+    .setLicense(
+      'Developed by Filipe Abrantes',
+      'https://github.com/filipenabrantes',
+    )
     .build();
-
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'inGaia Challenge',
+    customfavIcon: cssSwagger,
+    customCss: cssSwagger,
+  };
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, customOptions);
   await app.listen(process.env.PORT || 3000);
   Logger.log(`🚀 Running on ${process.env.PORT || 3000} port.`);
 }
