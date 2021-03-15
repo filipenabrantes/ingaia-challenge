c13d90a00a85b2f8b1c97f61a3a62b03b3dc621a
Filipe Abrantes <filipenabrantes@gmail.com>
2021-03-15 11:13:06 -0300

    adding unit and e2e tests

--- test/square-meter.e2e-spec.ts
+++ test/square-meter.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('SquareMeterController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/meter-value (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/meter-value')
       .expect(200)
-      .expect('Hello World!');
+      .expect('2000');
   });
 });
